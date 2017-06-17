8bff45fae5482d1ad58582040fdcf4f932ec3c6c
马鹏 <peng.ma@midea.com>
2017-06-17 14:14:32 +0800

    no message

--- src/components/demo/Demo2.vue
+++ src/components/demo/Demo2.vue
@@ -34,9 +34,13 @@ export default {
         components: {
             XHeader
         },
+        //mapGetters 辅助函数仅仅是将 store 中的 getters 映射到局部计算属性
         computed: mapGetters([
             'evenOrOdd'
         ]),
+        //Action 类似于 mutation，不同在于：
+        //Action 提交的是 mutation，而不是直接变更状态。
+        //Action 可以包含任意异步操作。
         methods: mapActions([
             'increment',
             'decrement',
